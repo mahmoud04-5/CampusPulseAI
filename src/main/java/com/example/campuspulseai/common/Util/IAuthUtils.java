cbb8614188a605b009af44365b2938ca23b66fe9
Abo-hashish <152917883+Abo-hashish@users.noreply.github.com>
2025-08-07 16:08:06 +0300

    added authentication util

--- src/main/java/com/example/campuspulseai/common/Util/IAuthUtils.java
+++ src/main/java/com/example/campuspulseai/common/Util/IAuthUtils.java
@@ -1,4 +1,11 @@
 package com.example.campuspulseai.common.Util;
 
+
+
+import com.example.campuspulseai.southBound.entity.User;
+
+import java.nio.file.AccessDeniedException;
+
 public interface IAuthUtils {
+    User getAuthenticatedUser() throws AccessDeniedException;
 }
