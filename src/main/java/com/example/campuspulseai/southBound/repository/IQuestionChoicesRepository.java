658e1f53da5c865f3326baf8cabd2c49159ec669
Abo-hashish <152917883+Abo-hashish@users.noreply.github.com>
2025-08-10 13:36:53 +0300

    resolved merge request issues

--- src/main/java/com/example/campuspulseai/southBound/repository/IQuestionChoicesRepository.java
+++ src/main/java/com/example/campuspulseai/southBound/repository/IQuestionChoicesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ISurveyRepository extends CrudRepository<QuestionChoices, Long> {
+public interface IQuestionChoicesRepository extends CrudRepository<QuestionChoices, Long> {
 }
