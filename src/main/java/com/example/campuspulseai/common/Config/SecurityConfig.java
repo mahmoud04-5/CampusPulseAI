aff8b04553b62fac4b8f23d0d0838f59afa08bce
Abo-hashish <152917883+Abo-hashish@users.noreply.github.com>
2025-08-10 11:32:08 +0300

    cleaned the secuirityConfig file

--- src/main/java/com/example/campuspulseai/common/Config/SecurityConfig.java
+++ src/main/java/com/example/campuspulseai/common/Config/SecurityConfig.java
@@ -59,20 +59,6 @@ public class SecurityConfig {
             response.getWriter().write("{\"error\": \"Unauthorized access\"");
         };
     }
-
-    //this bean is used to manage user details in the database and to define the queries for the custom tables in the db
-//    @Bean
-//    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-//        jdbcUserDetailsManager
-//                .setUsersByUsernameQuery("select id, password, active from system_users where user_id=? ");
-//
-//        jdbcUserDetailsManager
-//                .setAuthoritiesByUsernameQuery("select id, role from roles where user_id=?");
-//
-//        return jdbcUserDetailsManager;
-//    }
-
     //this bean is used to configure the security filter chain to apply the security rules for each endpoint and general security rules
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
