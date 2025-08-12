fe919aec229f22f33ca5afc27d3a38e5af67f5c4
Mahmoud ElAnsary <mahmoudansari2004@gmail.com>
2025-08-12 12:17:48 +0300

    all comments resolved

---
+++ src/main/java/com/example/campuspulseai/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.campuspulseai.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
