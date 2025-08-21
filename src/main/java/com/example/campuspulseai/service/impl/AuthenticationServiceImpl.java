4b89b774942e5339922045e6e6b93097640c06cb
Abo-hashish <152917883+Abo-hashish@users.noreply.github.com>
2025-08-21 13:38:31 +0300

    configured the email service and added a welcome email when a new user registers

--- src/main/java/com/example/campuspulseai/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/example/campuspulseai/service/impl/AuthenticationServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.campuspulseai.domain.dto.request.AuthenticationRequest;
 import com.example.campuspulseai.domain.dto.request.RegisterRequest;
 import com.example.campuspulseai.domain.dto.response.LoginResponse;
 import com.example.campuspulseai.service.IAuthenticationService;
+import com.example.campuspulseai.service.IEmailService;
 import com.example.campuspulseai.southbound.entity.User;
 import com.example.campuspulseai.southbound.mapper.AuthMapper;
 import com.example.campuspulseai.southbound.repository.IUserRepository;
@@ -13,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
@@ -24,13 +26,16 @@ public class AuthenticationServiceImpl implements IAuthenticationService {
     private final AuthMapper authenticationMapper;
     private final AuthenticationManager authenticationManager;
     private final JwtServiceImpl jwtService;
+    private final IEmailService emailService;
 
     @Override
+    @Transactional
     public void register(RegisterRequest registerRequest) throws Exception {
 
         registerRequest.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         User user = authenticationMapper.mapToUser(registerRequest);
         userRepository.save(user);
+        sendWelcomeEmail(user);
     }
 
     @Override
@@ -47,6 +52,12 @@ public class AuthenticationServiceImpl implements IAuthenticationService {
         String jwtToken = jwtService.generateToken(user);
 
         return authenticationMapper.mapToLoginResponse(user, jwtToken);
+    }
+
+    private void sendWelcomeEmail(User user) {
+        String subject = "Welcome to Campus Pulse AI!";
+        String body = "Dear " + user.getFirstName() + " " + user.getLastName() + ",\n\nThank you for registering with Campus Pulse AI. We are excited to have you on board!\n\nBest regards,\nCampus Pulse AI Team";
+        emailService.sendEmail(user.getEmail(), subject, body);
     }
 
 }
