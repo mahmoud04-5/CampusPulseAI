3d157b19f4b3a78bdbd5236f68e467bda5e11489
Abo-hashish <152917883+Abo-hashish@users.noreply.github.com>
2025-08-24 14:44:03 +0300

    resolved the merge issues

--- src/main/java/com/example/campuspulseai/service/impl/CloudinaryUploadServiceImpl.java
+++ src/main/java/com/example/campuspulseai/service/impl/CloudinaryUploadServiceImpl.java
@@ -5,6 +5,7 @@ import com.cloudinary.utils.ObjectUtils;
 import com.example.campuspulseai.domain.dto.response.FileUploadResponse;
 import com.example.campuspulseai.service.ICloudinaryUploadService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -20,25 +21,36 @@ import java.util.Map;
 public class CloudinaryUploadServiceImpl implements ICloudinaryUploadService {
 
     private final Cloudinary cloudinary;
+    private static final long MAX_FILE_SIZE = (long) 5 * 1024 * 1024; // 5 MB
 
     public FileUploadResponse uploadFile(MultipartFile file) throws IOException {
-        if (file.isEmpty()) {
-            throw new IllegalArgumentException("File is empty!");
-        }
 
-        String contentType = file.getContentType();
-        if (!(MediaType.IMAGE_JPEG_VALUE.equals(contentType) || MediaType.IMAGE_PNG_VALUE.equals(contentType))) {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Only JPG and PNG files are allowed!");
-        }
+        validateFileExistance(file);
+        validateFileType(file);
+        validateFileSize(file);
 
-        long maxSize = 5 * 1024 * 1024; // ie 5 mb
-        if (file.getSize() > maxSize) {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "File size exceeds the 5 MB limit!");
-        }
-        
         Map uploadResult = cloudinary.uploader().upload(file.getBytes(),
                 ObjectUtils.asMap("resource_type", "auto"));
 
         return new FileUploadResponse(uploadResult.get("secure_url").toString());
+    }
+
+    private void validateFileExistance(MultipartFile file) throws BadRequestException {
+        if (file.isEmpty()) {
+            throw new BadRequestException("File is empty!");
+        }
+    }
+
+    private void validateFileType(MultipartFile file) {
+        String contentType = file.getContentType();
+        if (!(MediaType.IMAGE_JPEG_VALUE.equals(contentType) || MediaType.IMAGE_PNG_VALUE.equals(contentType))) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Only JPG and PNG files are allowed!");
+        }
+    }
+
+    private void validateFileSize(MultipartFile file) {
+        if (file.getSize() > MAX_FILE_SIZE) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "File size exceeds the 5 MB limit!");
+        }
     }
 }
