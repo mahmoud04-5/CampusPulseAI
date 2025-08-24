e898326bf9af48d4dd6431e4f4b6c64804117598
Abo-hashish <152917883+Abo-hashish@users.noreply.github.com>
2025-08-24 16:07:16 +0300

    ensured requests are taken in request bodies not as query parameters

--- src/main/java/com/example/campuspulseai/domain/dto/request/RequestOTPRequest.java
+++ src/main/java/com/example/campuspulseai/domain/dto/request/RequestOTPRequest.java
@@ -2,21 +2,15 @@ package com.example.campuspulseai.domain.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
+@Getter
 @AllArgsConstructor
-public class AuthenticationRequest {
-
+public class RequestOTPRequest {
     @Email
     @NotBlank(message = "Email is required")
     private String email;
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, max = 30, message = "Password must be between 8 and 30 characters")
-    private String password;
 }
--- src/main/java/com/example/campuspulseai/northbound/controller/AuthenticationController.java
+++ src/main/java/com/example/campuspulseai/northbound/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.example.campuspulseai.northbound.controller;
 
 import com.example.campuspulseai.domain.dto.request.AuthenticationRequest;
 import com.example.campuspulseai.domain.dto.request.RegisterRequest;
+import com.example.campuspulseai.domain.dto.request.RequestOTPRequest;
 import com.example.campuspulseai.domain.dto.request.VerifyOtpRequest;
 import com.example.campuspulseai.domain.dto.response.LoginResponse;
 import com.example.campuspulseai.service.IAuthenticationService;
@@ -31,15 +32,15 @@ public class AuthenticationController {
     @Operation(summary = "Login a user", description = "Authenticates a user and returns an authentication response.")
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/login")
-    public LoginResponse login(AuthenticationRequest authRequest) {
+    public LoginResponse login(@Valid @RequestBody AuthenticationRequest authRequest) {
         return authenticationService.login(authRequest);
     }
 
     @PostMapping("/forgot-password/request-otp")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Request OTP", description = "Sends a One-Time Password (OTP) to the user's email to reset the password.")
-    public void requestOTP(String email) throws Exception {
-        authenticationService.requestOtp(email);
+    public void requestOTP(@Valid @RequestBody RequestOTPRequest requestOTPRequest) throws Exception {
+        authenticationService.requestOtp(requestOTPRequest.getEmail());
     }
 
     @PostMapping("/forgot-password/verify-otp")
@@ -53,7 +54,7 @@ public class AuthenticationController {
     @PostMapping("/forgot-password/reset")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Reset Password", description = "Resets the user's password using the provided OTP.")
-    public void resetPassword(String email, String newPassword) throws Exception {
-        authenticationService.resetPassword(email, newPassword);
+    public void resetPassword(@Valid @RequestBody AuthenticationRequest authRequest) throws Exception {
+        authenticationService.resetPassword(authRequest.getEmail(), authRequest.getPassword());
     }
 }
